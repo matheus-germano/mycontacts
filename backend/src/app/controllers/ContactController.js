e567d15f25fd84b84c9a311151fdb01b6a51e952
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-03-09 13:20:05 -0300

    :sparkles: feat: contact controller show method

--- backend/src/app/controllers/ContactController.js
+++ backend/src/app/controllers/ContactController.js
@@ -2,14 +2,20 @@ const ContactsRepository = require('../repositories/ContactsRepository');
 
 class ContactController {
   async index(request, response) {
-    // Listar todos os registros
     const contacts = await ContactsRepository.findAll();
 
     response.json(contacts);
   }
 
-  show() {
-    // Obter UM registro
+  async show(request, response) {
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    response.json(contact);
   }
 
   store() {
--- backend/src/app/repositories/ContactsRepository.js
+++ backend/src/app/repositories/ContactsRepository.js
@@ -8,12 +8,27 @@ const contacts = [
     phone: '19971171341',
     category_id: v4(),
   },
+  {
+    id: v4(),
+    name: 'Joao',
+    email: 'joao@gmail.com',
+    phone: '19971171341',
+    category_id: v4(),
+  },
 ];
 
 class ContactsRepository {
   findAll() {
     return new Promise((resolve) => {
       resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(
+        contacts.find((contact) => contact.id === id),
+      );
     });
   }
 }
