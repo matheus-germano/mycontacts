0c2942a985f54563b96593a525ce05d3a20f6ec1
Matheus Germano da Costa <dev.mgermano@gmail.com>
2023-03-09 13:00:34 -0300

    :heavy_plus_sign: build: changing uuidv4 for uuid

--- backend/src/app/repositories/ContactsRepository.js
+++ backend/src/app/repositories/ContactsRepository.js
@@ -1,12 +1,12 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
 const contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Matheus',
     email: 'matheus@gmail.com',
     phone: '19971171341',
-    category_id: uuid(),
+    category_id: v4(),
   },
 ];
 
